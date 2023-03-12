bb4e17968f527f20745c8313c7b03a96f6caeb5b
charismacinta <cintacharisma1@gmail.com>
2023-03-12 19:47:28 +0700

    tugas prokom 1

---
+++ exercise-01.py
@@ -0,0 +1,5 @@
+print("Nama: Charisma Cintani Adhi Surya")
+print("NIM: I0322027")
+
+print("     Saya tinggal di Kabupaten Karanganyar tepatnya di Jalan Nusama, Ngringo, Jaten. Menurut saya rumah saya cukup nyaman dan strategis. Di belakang rumah saya terdapat pemandangan sawah yang cukup cantik dan juga Gunung Lawu yang gagah. Rumah saya dekat dengan ringroad sehingga ketika pergi berkuliah saya hanya memerlukan waktu kurang lebih 10 menit. Saya senang tinggal di Palur, walaupun masih berada di kawasan pedesaan namun jika ingin berpergian ke Solo ataupun ke Karanganyar Kota jaraknya tidak terlalu jauh. ")
+print("     Harapan saya setelah lulus dan menjadi sarjana Teknik Industri adalah saya mampu menjaga nama baik TI UNS dan saya berharap saya dapat bekerja di Astra/Nasmoco suatu saat nanti. Selain itu saya juga memiliki impian untuk memiliki bisnis yang sukses dan tentunya dapat mempekerjakan orang-orang yang membutuhkan pekerjaan diluar sana.")
